6bae0535f3507186e80f9b8816a66e01ae8bd7ab
Vadim26kt <ka.vadim2016@yandex.ru>
2022-05-05 19:50:57 +0300

    Решение домашнего задания

---
+++ Lesson1/src/main/java/Task2.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Task2 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int  size = in.nextInt();
+        int count =0;
+        if (size > 0) {
+            count++;
+        }
+    System.out.println(count) ;
+        }
+
+
+        }
+
