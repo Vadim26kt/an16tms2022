6961a939c0752e2f590b6acb8782b0d96c505cef
Vadim26kt <ka.vadim2016@yandex.ru>
2022-05-09 14:20:55 +0300

    Часть решение домашнего задания 2.

---
+++ Lesson2/src/main/java/Lesson2.java
@@ -0,0 +1,17 @@
+public class Lesson2 {
+    public static void main(String[] args) {
+        boolean c = false;
+        boolean d = true;
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println("String literal");
+        System.out.println('a');
+        System.out.println(0b1101);
+        System.out.println(012);
+        System.out.println(0x1A);
+        System.out.println(12);
+        System.out.println(12.5F);
+        System.out.println(12e-3);
+    }
+
+}
